d11bc2e2559a0e59b67b3e95d60c4786e2a1f326
Le Trong Anh Tu <88422176+letronganhtu@users.noreply.github.com>
2022-03-08 10:29:33 +0700

    Update Colors.py

--- Colors.py
+++ Colors.py
@@ -1,9 +1,8 @@
-#color
 default_color = 'grey'
 current_color = 'orange'
 visited_color = 'deepskyblue'
 path_node_color = 'lime'
-queue_color='violet'
+queue_color = 'violet'
 path_color = 'red'
-dep_color='lightgreen'
-des_color='lightgreen'
+dep_color = 'lightgreen'
+des_color = 'lightgreen'
